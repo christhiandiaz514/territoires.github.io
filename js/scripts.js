85bffde6146d54cf024ae907897d3116e1ee4e0e
Étienne Després <etienne@pochour.com>
2014-07-08 16:48:13 -0400

    Shimmage de logo et de lignes topographiques.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ document.onload = function() {
   var logo = document.getElementById('logo');
 }
 
-window.onscroll = function (event) {
-  if ((window.pageYOffset || document.documentElement.scrollTop) > 0) {
+window.onscroll = function(event) {
+  var windowOffset = window.pageYOffset || document.documentElement.scrollTop;
+  if (windowOffset > 30) {
     logo.className = "sticky";
   } else {
     logo.className = "";
   }
+  logo.style.backgroundPosition = "0 -" + Math.round(windowOffset / 2) + "px";
 }
