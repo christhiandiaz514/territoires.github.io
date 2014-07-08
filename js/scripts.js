372cd69ec175bfbfdfc6b1edf53fc553c6278a9d
Étienne Després <etienne@pochour.com>
2014-07-08 17:52:12 -0400

    Fuck off, j'inclus jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-document.onload = function() {
+$(function() {
   var logo = document.getElementById('logo');
-}
+  
+  // Quand on clique sur le logo en page d'accueil, scroll up au lieu de reload
+  $('body.home #logo a').click(function() {
+    $('html,body').animate({ scrollTop: 0 }, 'fast');
+    return false;
+  });
+});
 
 window.onscroll = function(event) {
   var windowOffset = window.pageYOffset || document.documentElement.scrollTop;
